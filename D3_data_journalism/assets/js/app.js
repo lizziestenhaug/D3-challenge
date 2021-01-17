790aa037c5d5b2f42110db5a8a87b5ad432e0bfd
Lizzie Stenhaug <lizziestenhaug@Lizzies-MacBook-Pro.local>
2021-01-17 16:09:47 -0600

    updated code

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,12 +1,12 @@
 //chart set up
-var svgwidth = 960;
+var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-    top: 20,
-    right: 40,
-    bottom: 80,
-    left:100
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
@@ -14,89 +14,92 @@ var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
-
 var svg = d3
-  .select(".scatter")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-  // Append an SVG group
+// Append an SVG group
 var chartGroup = svg.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Data Import
 d3.csv("assets/data/data.csv").then(function(CensusData) {
-    CensusData.forEach(function(data) {
-        data.age = +data.age;
-        data.smokes = +data.smokes;
-        console.log(data);
-    });
+  CensusData.forEach(function(data) {
+    data.age = +data.age;
+    data.smokes = +data.smokes;
+  });
 
     //function used for updating x-scale
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(CensusData, d => d.age))
-        .range([0, width]);
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(CensusData, d => d.age))
+    .range([0, width]);
+ 
 
-    const yScale = d3.scaleLinear()
-        .domain([6,d3.max(CensusData, d => d.smokes)])
-        .range([height, 0]);
-
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-
-    //append axes
-    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
-    chartGroup.append("g").call(yAxis);
-
-    //scatter plot
-    chartGroup.selectAll("circle")
-    .data(CensusData)
-    .enter()
-    .append("circle")
-    .attr("cx", d=>xScale(d.age))
-    .attr("cy", d=>yScale(d.smokes))
-    .attr("r", 20)
-    .attr("fill", "blue")
-    .attr("opacity", ".75");
-
-    //add texts
-    chartGroup.append("g")
-    .selectAll('text')
-    .data(CensusData)
-    .enter()
-    .append("text")
-    .text(d=>d.abbr)
-    .attr("x",d=>xScale(d.age))
-    .attr("y", d=>yScale(d.smokes))
-    .classed(".stateText", true)
-    .attr("font-family", "sans-serif")
-    .attr("text-anchor", "middle")
-    .attr("font-size", "10px")
-    .style("font-size", "bold")
-    .attr("alignment-baseline", "central");   
+  const yScale = d3.scaleLinear()
+    .domain([6,d3.max(CensusData, d => d.smokes)])
+    .range([height, 0]);
     
-    //axis titles
-    chartGroup.append("text")
+  
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+
+  //append axes
+  chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+  chartGroup.append("g").call(yAxis);
+
+
+chartGroup.selectAll("circle")
+.data(CensusData)
+.enter()
+.append("circle")
+.attr("cx", d=>xScale(d.age))
+.attr("cy", d=>yScale(d.smokes))
+.attr("r", "10")
+.attr("stroke-width", "1")
+.classed("stateCircle", true)
+.attr("opacity", 0.75);
+
+
+chartGroup.append("g")
+  .selectAll('text')
+  .data(CensusData)
+  .enter()
+  .append("text")
+  .text(d=>d.abbr)
+  .attr("x",d=>xScale(d.age))
+  .attr("y",d=>yScale(d.smokes))
+  .classed(".stateText", true)
+  .attr("font-family", "sans-serif")
+  .attr("text-anchor", "middle")
+  .attr("fill", "black")
+  .attr("font-size", "10px")
+  .style("font-weight", "bold")
+  .attr("alignment-baseline", "central");
+  
+  
+  chartGroup.append("text")
         .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
         .attr("text-anchor", "middle")
-        .attr("font-size", "18px")
-        .attr("fill", "Navy")
+        .attr("font-size", "16px")
+        .attr("fill", "Black")
         .style("font-weight", "bold")
         .text("Median Age");
 
         chartGroup.append("text")
-            .attr("y", 0 - ((margin.left / 2) + 2))
-            .attr("x", 0 - (height /2))
-            .attr("text-anchor", "middle")
-            .attr("font-size", "18px")
-            .attr("fill", "navy")
-            .style("font-weight", "bold")
-            .attr("transform", "rotate(-90)")
-            .text("Smokers (%)")
-   }).catch(function(error) {
-       console.log(error);
-   });
+        .attr("y", 0 - ((margin.left / 2) + 2))
+        .attr("x", 0 - (height / 2))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "16px")
+        .attr("fill", "black")
+        .style("font-weight", "bold")
+        .attr("transform", "rotate(-90)")
+        .text("Smokers (%)");
+}).catch(function(error) {
+  console.log(error);
+});
 
 
 
