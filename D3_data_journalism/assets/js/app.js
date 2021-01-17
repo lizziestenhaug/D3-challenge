23fb7374fa37cf5a24b456e2ea5cbcf0445c5093
Lizzie Stenhaug <lizziestenhaug@Lizzies-MacBook-Pro.local>
2021-01-17 12:15:47 -0600

    updated

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -44,9 +44,59 @@ d3.csv("assets/data/data.csv").then(function(CensusData) {
 
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
-        
+
+    //append axes
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(xAxis);
+    chartGroup.append("g").call(yAxis);
+
+    //scatter plot
+    chartGroup.selectAll("circle")
+    .data(CensusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d=>xScale(d.age))
+    .attr("cy", d=>yScale(d.smokes))
+    .attr("r", 20)
+    .attr("fill", "blue")
+    .attr("opacity", ".75");
+
+    //add texts
+    chartGroup.append("g")
+    .selectAll('text')
+    .data(CensusData)
+    .enter()
+    .append("text")
+    .text(d=>d.abbr)
+    .attr("x",d=>xScale(d.age))
+    .attr("y", d=>yScale(d.smokes))
+    .classed(".stateText", true)
+    .attr("font-family", "sans-serif")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "10px")
+    .style("font-size", "bold")
+    .attr("alignment-baseline", "central");   
     
-});
+    //axis titles
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 13})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "18px")
+        .attr("fill", "Navy")
+        .style("font-weight", "bold")
+        .text("Median Age");
+
+        chartGroup.append("text")
+            .attr("y", 0 - ((margin.left / 2) + 2))
+            .attr("x", 0 - (height /2))
+            .attr("text-anchor", "middle")
+            .attr("font-size", "18px")
+            .attr("fill", "navy")
+            .style("font-weight", "bold")
+            .attr("transform", "rotate(-90)")
+            .text("Smokers (%)")
+   }).catch(function(error) {
+       console.log(error);
+   });
 
 
 
